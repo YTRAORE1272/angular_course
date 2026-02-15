53123a77d6bd49a108c2da7503812f8d0f84d7cf
Youssouf <ytraore1272@gmail.com>
2026-02-15 00:05:50 +0000

    livrable-cour3

--- src/app/features/private/demande-rv/list-demande/list-demande.component.ts
+++ src/app/features/private/demande-rv/list-demande/list-demande.component.ts
@@ -1,21 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { DemandeListRvModel, StatusDemandeModel, SpecialiteModel} from '../../models/demande.model';
-
+import { FormsModule } from '@angular/forms';
+import { Demande, Specialite, Statut } from '../models/demande.model';
+import { DemandeFilter } from '../models/demande-filter.model';
+import { DemandeResponse } from '../models/demande-response.model';
+import { DemandeService } from '../services/demande.service';
 
 @Component({
   selector: 'app-list-demande',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './list-demande.component.html',
   styleUrl: './list-demande.component.css'
 })
-export class ListDemandeComponent {
- title: string = "Liste des demandes de rendez-vous";
- demandes: DemandeListRvModel[] = [
-   { id: 1, dateDemande: '2024-07-01', heure: '10:00', statut: StatusDemandeModel.EN_ATTENTE, specialite: SpecialiteModel.CARDIOLOGIE },
-   { id: 2, dateDemande: '2024-07-05', heure: '14:00', statut: StatusDemandeModel.ACCEPTEE, specialite: SpecialiteModel.DERMATOLOGIE },
-   { id: 3, dateDemande: '2024-07-10', heure: '09:00', statut: StatusDemandeModel.REFUSEE, specialite: SpecialiteModel.PEDIATRIE }
- ];
+export class ListDemandeComponent implements OnInit {
+  title: string = "Liste des demandes de rendez-vous";
 
- 
+  filter: DemandeFilter = {
+    statut: '',
+    specialite: '',
+  };
+
+  response: DemandeResponse = {
+    data: [],
+    totalPage: 0,
+    currentPage: 1,
+    totalItems: 0,
+    pages: []
+  };
+
+  constructor(private demandeService: DemandeService) {}
+
+  ngOnInit(): void {
+    this.loadDemandes();
+  }
+
+  private loadDemandes(): void {
+    this.response = this.demandeService.getDemandes(this.filter, this.response.currentPage);
+  }
+
+  onFilterChange(): void {
+    this.response.currentPage = 1;
+    this.loadDemandes();
+  }
+
+  onPageChange(page: number): void {
+    this.response.currentPage = page;
+    this.loadDemandes();
+  }
 }
